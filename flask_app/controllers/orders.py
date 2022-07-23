0b05561f90ee844a6023e58b4b257fb67b417308
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2022-07-24 02:11:29 +0800

    completed order_new.html

--- flask_app/controllers/orders.py
+++ flask_app/controllers/orders.py
@@ -3,5 +3,22 @@ from flask import render_template,redirect, request,session,flash
 from flask_app.models.order import Order
 
 @app.route('/')
-def index():
+def orders():
     return render_template('orders.html',orders=Order.get_all())
+
+@app.route('/order_new/')
+def order_new():
+    return render_template('order_new.html')
+
+@app.route('/order_save/')
+def order_save():
+    return redirect('/')
+
+@app.route('/order_edit/<int:order_id>/')
+def order_edit(order_id):
+    return render_template('order_edit.html')
+
+@app.route('/order_update/<int:order_id>/')
+def order_update(order_id):
+    return redirect('/')
+
